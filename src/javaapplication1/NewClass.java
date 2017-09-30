f9bbd273d040da3bee3ddff64982d3eba51ff690
tlacastiel <tlacaelel.romo@gmail.com>
2017-09-30 16:17:43 -0500

    Se agregan clases respectivas

---
+++ src/javaapplication1/NewClass.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author erik
+ */
+public class NewClass {
+    private String asd;
+    
+    private String marca;
+
+    public String getAsd() {
+        return asd;
+    }
+
+    public void setAsd(String asd) {
+        this.asd = asd;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+    
+    public static void main(String[] args) {
+        NewClass asd = new NewClass();
+        asd.setAsd("AFria");
+        System.out.println("PRINT="+asd.getAsd());        
+    }
+}
