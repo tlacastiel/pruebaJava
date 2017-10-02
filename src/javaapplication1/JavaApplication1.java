321aedb7a8ee294fdf731ac52493af09ba17089b
tlacastiel <tlacaelel.romo@gmail.com>
2017-10-02 12:21:55 -0500

    Se agregan cambios a clase wind 7

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -16,6 +16,9 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.print("HOLA WORLDS");
+        // TODO code application logic here
+        System.out.print("HOLA WORLDS22");
     }
     
 }
